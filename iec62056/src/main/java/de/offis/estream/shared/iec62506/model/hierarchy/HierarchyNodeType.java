5940f8657018a40eb575dd5efdd4122614b4dc56
Tim Silhan <Tim.Silhan@uni-oldenburg.de>
2018-03-06 15:14:51 +0100

    Added new node types to fit the hierarchy

--- iec62056/src/main/java/de/offis/estream/shared/iec62506/model/hierarchy/HierarchyNodeType.java
+++ iec62056/src/main/java/de/offis/estream/shared/iec62506/model/hierarchy/HierarchyNodeType.java
@@ -2,8 +2,10 @@ package de.offis.estream.shared.iec62506.model.hierarchy;
 
 public enum HierarchyNodeType {
     // add other types here, js runtime has no real reflection
-    NODE,
+    ROOT,
+    SUBGRID,
     TRANSFORMATOR,
+    NODE,
     SMGW,
     SMART_METER;
 }
